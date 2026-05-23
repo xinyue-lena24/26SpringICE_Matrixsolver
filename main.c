1756f28138ca45f5c5917365e0756e8fb76bf883
zhangpanyang <azhangpanyang@126.com>
2026-05-23 17:24:21 +0800

    modify comments

--- main.c
+++ main.c
@@ -69,7 +69,7 @@ void test_multiply()
 
         int scale = n * m * p;
         printf("Scale (n*m*p): %d\n", scale);
-        int test_num = (1e8 / scale) + 5; // Adjust test_num based on the scale to keep total operations around 1e8
+        int test_num = (1e8 / scale) + 10; // Adjust test_num based on the scale
         printf("Running multiplication test %d times...\n", test_num);
 
         timer_start(&timer);
