f390498330c681a7b4265e592a237f8498e820a7
zhangpanyang <azhangpanyang@126.com>
2026-05-23 18:14:53 +0800

    i k j

--- src/core/matrix_ops.c
+++ src/core/matrix_ops.c
@@ -96,9 +96,10 @@ MatrixError MatrixMultiply(const Matrix *A, const Matrix *B, Matrix *C)
         return MATRIX_ERROR_SIZE_MISMATCH;
     }
     MatrixFillZero(C);
-    for (int i = 0; i < A->row; ++i) { // i j k
-        for (int j = 0; j < B->column; ++j) {
-            for (int k = 0; k < A->column; ++k) {
+    // i k j
+    for (int i = 0; i < A->row; ++i) {
+        for (int k = 0; k < A->column; ++k) {
+            for (int j = 0; j < B->column; ++j) {
                 C->data[MatrixIndex(C, i, j)] += A->data[MatrixIndex(A, i, k)] * B->data[MatrixIndex(B, k, j)];
             }
         }
