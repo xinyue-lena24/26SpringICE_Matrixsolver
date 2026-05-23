950755f83146c5ee2ef06ea73c4c6543ed2323fe
zhangpanyang <azhangpanyang@126.com>
2026-05-23 17:34:46 +0800

    new scale

--- main.c
+++ main.c
@@ -21,12 +21,17 @@ testScale test_scales[31*31*31];
 
 void init_test_scales() {
     srand(0); // Reset random seed for reproducibility
-    test_num = 0;
+
+    // test scales required
+    test_scales[0] = (testScale){100, 100, 100};
+    test_scales[1] = (testScale){1000, 1000, 1000};
+    test_scales[2] = (testScale){2000, 2000, 2000};
+    test_num = 3;
 
     // Generate test scales with n, m, p
-    for (int n = 16; n <= 1024; n *= 4) {
-        for (int m = 16; m <= 1024; m *= 4) {
-            for (int p = 16; p <= 1024; p *= 4) {
+    for (int n = 32; n <= 2048; n *= 4) {
+        for (int m = 32; m <= 2048; m *= 4) {
+            for (int p = 32; p <= 2048; p *= 4) {
                 test_scales[test_num].n = n;
                 test_scales[test_num].m = m;
                 test_scales[test_num].p = p;
