ecac35db1d9faee0aad705d1dda07b1a33e5d8ac
zhangpanyang <azhangpanyang@126.com>
2026-05-24 13:05:35 +0800

    create correctness_verification function

--- main.c
+++ main.c
@@ -191,12 +191,36 @@ void test_all_four_operations()
     test_multiply();
 }
 
+void correctness_verification()
+{
+    Matrix A, B, C;
+    MatrixInit(&A); MatrixInit(&B); MatrixInit(&C);
+    MatrixCreate(&A, 3, 3); MatrixCreate(&B, 3, 3);
+    MatrixFillSequence(&A, 1.0, 1.0); // A = [1, 2, 3; 4, 5, 6; 7, 8, 9]
+    MatrixFillSequence(&B, 9.0, -1.0); // B = [9, 8, 7; 6, 5, 4; 3, 2, 1]
+    MatrixCreate(&C, 3, 3);
+
+    Check(MatrixAdd(&A, &B, &C), "MatrixAdd");
+    MatrixPrint(&C, "A + B");
+    Check(MatrixScale(2.0, &A, &C), "MatrixScale");
+    MatrixPrint(&C, "2 * A");
+    Check(MatrixTranspose(&A, &C), "MatrixTranspose");
+    MatrixPrint(&C, "A^T");
+    Check(MatrixMultiply(&A, &B, &C), "MatrixMultiply");
+    MatrixPrint(&C, "A * B");
+
+    MatrixFree(&A); MatrixFree(&B); MatrixFree(&C);
+}
+
 int main(void)
 {
     init_test_scales();
 
+    // correctness verification
+    correctness_verification();
+
     // test all four functions
-    test_all_four_operations();
+    // test_all_four_operations();
 
     // test multiply
     // test_multiply();
